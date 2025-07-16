b9d4ad7e4d8b0642e13c00b859381822e8dd230b
s2a0v2a4 <xcrush389@gmail.com>
2025-07-16 15:57:32 +0200

    Read me

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
   process.env.PORT = '5000';
   process.env.FRONTEND_URL = 'http://localhost:3000';
-  process.env.CORS_ORIGINS = 'http://localhost:3000,http://127.0.0.1:3000';
+  process.env.CORS_ORIGINS = 'http://localhost:3000,http://127.0.0.1:3000', "http://172.17.212.179:3000";
   process.env.LOG_LEVEL = 'debug';
 }
 
